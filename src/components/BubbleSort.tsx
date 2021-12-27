4c4e40bd748546fa5b67d786e5484828affd02b9
Gustavo Muñoz <timbergus@gmail.com>
2021-12-27 03:54:09 +0100

    Update to modern React.

---
+++ src/components/BubbleSort.tsx
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+const ButtonStyled = styled.button`
+  margin-bottom: 20px;
+  margin-right: 20px;
+  background-color: ${({ disabled }): string =>
+    disabled ? '#CCCCCC' : '#4CAF50'};
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: ${({ disabled }): string => (disabled ? 'auto' : 'pointer')};
+`;
+
+type BarStyledProps = {
+  $length: number;
+};
+
+const BarStyled = styled.div<BarStyledProps>`
+  width: ${({ $length }): number => $length}px;
+  height: 5px;
+  background-color: cadetblue;
+`;
+
+const shuffle = (arr: number[]) => {
+  arr.sort(() => Math.random() - 0.5);
+};
+
+const BubbleSort = () => {
+  const [lengths, setLengths] = useState<number[]>([]);
+  const [sorting, setSorting] = useState(false);
+
+  useEffect(() => {
+    const lengths = [];
+    for (let i = 0; i < 100; i++) {
+      lengths.push((i + 1) * 5);
+    }
+    shuffle(lengths);
+    setLengths([...lengths]);
+  }, []);
+
+  const reset = () => {
+    shuffle(lengths);
+    setLengths([...lengths]);
+  };
+
+  const swap = (a: number, b: number) =>
+    new Promise<boolean>((resolve) =>
+      setTimeout(() => {
+        let tmp = lengths[a];
+        lengths[a] = lengths[b];
+        lengths[b] = tmp;
+        setLengths([...lengths]);
+        resolve(true);
+      }, 0),
+    );
+
+  const bubbleSort = async () => {
+    let changed = false;
+    setSorting(true);
+    do {
+      changed = false;
+      for (let i = 0; i < lengths.length - 1; i++) {
+        if (lengths[i] > lengths[i + 1]) {
+          changed = await swap(i, i + 1);
+        }
+      }
+    } while (changed);
+    setSorting(false);
+  };
+
+  return (
+    <div>
+      <ButtonStyled onClick={() => bubbleSort()} disabled={sorting}>
+        Bubble Sort
+      </ButtonStyled>
+      <ButtonStyled onClick={() => reset()} disabled={sorting}>
+        Reset
+      </ButtonStyled>
+      {lengths.map((length: number) => (
+        <BarStyled key={length} $length={length} />
+      ))}
+    </div>
+  );
+};
+
+export default BubbleSort;
--- src/components/bar.styled.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import styled from 'styled-components';
-
-export default styled.div`
-  width: ${props => props.length}px;
-  height: 5px;
-  background-color: cadetblue;
-`;
